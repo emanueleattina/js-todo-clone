5f82e6c2b0aa1d56584a8a901cbfd9747279a54c
emanuele <emattina97@gmail.com>
2021-03-17 17:54:30 +0100

    Carico esercizio completo

--- main.js
+++ main.js
@@ -7,61 +7,63 @@ $(document).ready(function () {
         var template = $('#fare .new-task').clone();
         template.children('h3.task').text(inserisci);
 
-        template.children('#fatto').click(function () {
-            $(this).siblings('.task').addClass('fatto');
-            console.log('s');
+        template.children('.bottoni').children('#fatto').click(function () {
+            $(this).parent('.bottoni').siblings('.task').addClass('completato');
 
+            $(this).hide();
+            $(this).siblings('#edit').hide();
+
+            $(this).siblings('#canc').show();
         });
 
-        template.children('#edit').click(function () {
-            var titleDOM = $(this).siblings('.task');
-            var title = titleDOM.text();
+        template.children('.bottoni').children('#canc').click(function () {
+            $(this).parent('.bottoni').parent('.new-task').remove();
+        });
 
+
+
+
+        template.children('.bottoni').children('#edit').click(function () {
+
+            var titleDOM = $(this).parent('.bottoni').siblings('.task');
+            var title = titleDOM.text();
             titleDOM.hide();
+
+            $(this).parents('.bottoni').parents('.new-task').addClass('mod');
+
+            $(this).siblings('.task').hide();
             $(this).siblings('#fatto').hide();
-            template.children('#edit').hide();
+            template.children('.bottoni').children('#edit').hide();
 
             $(this).siblings('#modFinita').show();
 
-            $(this).siblings('#inputEdit').val(title).show();
+            $(this).parent('.bottoni').siblings('input').val(title).show();
+
+            // $(this).parent('.bottoni').siblings('#inputEdit').val($(this).siblings('.task').text()).show();
         });
 
-        template.children('#modFinita').click(function () {
-            var title = $(this).siblings('#inputEdit').val();
 
-            $(this).siblings('h3.task').text(title).show();
-            $(this).siblings('#inputEdit').hide();
-            template.children('#modFinita').hide();
+        template.children('.bottoni').children('#modFinita').click(function () {
+
+            var title = $(this).parents('.bottoni').siblings('input').val();
+
+            $(this).parents('.bottoni').parents('.new-task').removeClass('mod');
+
+            $(this).parents('.bottoni').siblings('h3.task').text(title).show();
+
+
+            $(this).parent('.bottoni').siblings('#inputEdit').val($(this).siblings('.task').text()).hide();
+            template.children('.bottoni').children('#modFinita').hide();
 
             $(this).siblings('#fatto').show();
             $(this).siblings('#edit').show();
         });
 
+        
+
         $('#daFare').append(template);
-    });
-
-    
 
 
-
-
-
-
-
-    $('#inserisci').keyup(function (e) {
-
-        if (e.which == 13) {
-            var inserisci = $('#inserisci').val();
-            $('#inserisci').val("")
-
-            var template = $('#fare .new-task').clone();
-            template.children('h3.task').text(inserisci);
-
-            template.children('#fatto').click(function () {
-                $(this).siblings('.task').addClass('fatto');
-            });
-        }
-        $('#daFare').append(template);
     });
 
 });
